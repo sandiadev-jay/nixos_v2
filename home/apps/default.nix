e841a8d686ee965626d6294f565658468948114b
Jay Anderson <jay.anderson@sandiadev.com>
2024-07-10 22:16:51 -0600

    Swapping google-drive-ocamlfuse in for rclone

--- home/apps/default.nix
+++ home/apps/default.nix
@@ -43,7 +43,8 @@
     # nautilus  # GUI file browser (Gnome's)
     libreoffice  # Office suite
     pantheon.elementary-files  # GUI file browser with miller columns
-    rclone  # Sync utility for cloud drives (Google Drive)
+    # rclone  # Sync utility for cloud drives (Google Drive)
+    google-drive-ocamlfuse  # Sync utility for Google Drive
 
     # system call monitoring
     strace # system call monitoring
