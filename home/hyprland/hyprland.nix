c4302e11c6b4b09e31896d506c365df9258636a3
Jay Anderson <jay.anderson@sandiadev.com>
2024-08-02 12:31:39 -0600

    Switched to hybrid sleep when closing lid

--- home/hyprland/hyprland.nix
+++ home/hyprland/hyprland.nix
@@ -294,8 +294,8 @@
       ];
 
       bindl = [
-        ", switch:on:Lid Switch, exec, (pidof hyprlock || hyprlock) & (sleep 3 && hyprctl keyword monitor 'eDP-1, disable')"
-        ", switch:off:Lid Switch, exec, hyprctl keyword monitor 'eDP-1, preferred, -1729x1080, 2'"
+        ", switch:on:Lid Switch, exec, systemctl default"
+        ", switch:off:Lid Switch, exec, systemctl hybrid-sleep"
       ];
     };
   };
